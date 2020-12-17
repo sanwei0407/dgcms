2ea963ee4bfe18f584cd08fb525edb207cc0a9ba
qiyin <manongqiyin@163.com>
2020-12-17 09:01:48 +0800

    增加分类 类别的接口 Signed-off-by: qiyin <manongqiyin@163.com>

--- app/controller/client/serveType.js
+++ app/controller/client/serveType.js
@@ -23,21 +23,103 @@ class ServerTypeController extends Controller {
             success: false,
             msg: '请输入所属服务类型范围'
         }
+        if (serve == Pserve) return ctx.body = {
+            success: false,
+            msg: '类型跟父类型重复'
+        }
         try {
-            const res = await ctx.model.ServeType.create({
-                serve, // 服务类型
-                Pserve, // 所属服务类型
-                isDelete: 0, // 是否删除
-            })
-            ctx.body = {
-                success: true,
-                msg: '添加成功',
+            // 判断 有没有传入 Pserve
+            if (Pserve) {
+                // 去找是否有serve和Pserve一样 的服务
+                const typeRes = await ctx.model.Servetype.findOne({
+                    where: {
+                        serve: serve,
+                        Pserve: Pserve,
+                    }
+                })
+                // 如果找不到
+                if (typeRes == null) {
+                    // 判断有没有Pserve 的 服务
+                    let pp = await ctx.model.Servetype.findOne({
+                        where: {
+                            serve: Pserve
+                        }
+                    })
+                    // 如果有的话就可以添加
+                    if (pp) {
+                        try {
+                            let res = await ctx.model.Servetype.create({
+                                serve: serve,
+                                Pserve: Pserve,
+                                isDelete: 0
+                            })
+                            console.log(res)
+                            ctx.body = {
+                                success: true,
+                                msg: '添加成功',
+                                data: res
+                            }
+                        } catch (e) {
+                            console.log(e)
+                            ctx.body = {
+                                success: false,
+                                msg: '添加失败',
+                                error: e
+                            }
+                        }
+                    }else{
+                        ctx.body = {
+                            success: false,
+                            msg: '该父类的服务不存在',
+                        }
+                    }
+                } else {
+                    ctx.body = {
+                        success: false,
+                        msg: '类型已存在'
+                    }
+                }
+            } else {
+                const res = await ctx.model.Servetype.findOne({
+                    where: {
+                        serve: serve,
+                        Pserve: 0,
+                    }
+                })
+                if (res == null) {
+                    console.log('res == null')
+                    try {
+                        let res = await ctx.model.Servetype.create({
+                            serve: serve,
+                            Pserve: 0,
+                            isDelete: 0
+                        })
+                        console.log(res)
+                        ctx.body = {
+                            success: true,
+                            msg: '添加成功',
+                            data: res
+                        }
+                    } catch (e) {
+                        console.log(e)
+                        ctx.body = {
+                            success: false,
+                            msg: '添加失败',
+                            error: e
+                        }
+                    }
+                } else {
+                    ctx.body = {
+                        success: false,
+                        msg: '类型已存在'
+                    }
+
+                }
             }
         } catch (e) {
-            console.log(e)
             ctx.body = {
                 success: false,
-                msg: 'e' + e
+                msg: '添加失败' + e
             }
         }
     }
